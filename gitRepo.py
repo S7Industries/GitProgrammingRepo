e5adb97d1a91ea4242b5601dcab9ba59cf6c85b6
marcs7 <marcosatta9@gmail.com>
2020-06-28 21:16:25 +0200

    added print list data

--- gitRepo.py
+++ gitRepo.py
@@ -1,7 +1,26 @@
 from gitHandle import gitRepo
 from dataServer import *
+import getpass
 
-ssh = gitRepo(server[1],username[1], psw[1], remoteFolder[0]) #if you use array 
+print ('Server address registered: \n')
+for i in server:
+    print(str(server.index(i))+')'+i)
+
+print('Username registered: \n')
+for i in username:
+    print(str(username.index(i))+')'+i)
+
+print('Remote folder registered: \n')
+for i in remoteFolder:
+    print(str(remoteFolder.index(i))+')'+i)
+
+
+serverNumber = int(raw_input('Insert number of server to connect: '))
+usernameNumber = int(raw_input('Insert number of username to connect: '))
+remoteFolderNumber = int(raw_input('Insert number of remote folder to connect: '))
+password = getpass.getpass('Insert password: ')
+
+ssh = gitRepo(server[serverNumber],username[usernameNumber], psw[password], remoteFolder[remoteFolderNumber]) #if you use array 
 #ssh = gitRepo(server, username ,psw, remoteFolder) #if not use array 
 
 repoData = raw_input('insert repo name and language: ').split(' ')
